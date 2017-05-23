e3bc27d3aa3093689814ae77b819cc710ff1616a
15017541 <15017541@myrp.edu.sg>
2017-05-23 16:56:09 +0800

    Initial commit

---
+++ app/src/main/java/android/myapplicationdev/com/demodatabasecrud/Note.java
@@ -0,0 +1,31 @@
+package android.myapplicationdev.com.demodatabasecrud;
+
+import java.io.Serializable;
+
+/**
+ * Created by 15017541 on 23/5/2017.
+ */
+
+public class Note implements Serializable {
+
+    private int id;
+    private String noteContent;
+
+    public Note(int id, String noteContent) {
+        this.id = id;
+        this.noteContent = noteContent;
+    }
+
+    public int getId() { return id; }
+
+    public String getNoteContent() { return noteContent; }
+
+    public void setNoteContent(String noteContent) {
+        this.noteContent = noteContent;
+    }
+    @Override
+    public String toString() {
+        return "ID:" + id + ", " + noteContent;
+    }
+
+}
